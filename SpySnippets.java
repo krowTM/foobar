cd1b309b6ad1915ebf1d859c77937a08fe20a30c
krowTM <softdev14@gmail.com>
2016-08-06 10:46:25 +0300

    Indentation fix

--- SpySnippets.java
+++ SpySnippets.java
@@ -7,8 +7,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Answer {   
-    public static String answer(String document, String[] searchTerms) {
+public class Answer {
+   public static String answer(String document, String[] searchTerms) {
     	String ret = "";
     	Map<String, List<Integer>> wordsHash = new HashMap<String, List<Integer>>();
     	List<String> words = Arrays.asList(document.split("\\s+"));
